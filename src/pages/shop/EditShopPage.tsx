201ccaffe583d24348f48700b99d2fef22a37a95
olesia098 <olesya.kan.05@mail.ru>
2023-12-04 19:44:29 +0600

    crud u

--- src/pages/shop/EditShopPage.tsx
+++ src/pages/shop/EditShopPage.tsx
@@ -1,11 +1,80 @@
-import React from 'react';
+import { useProducts } from "context/products/ProductsContextProvider";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const EditShopPage = () => {
-    return (
-        <div>
-            edit
-        </div>
-    );
+  const { oneProduct, getOneProduct, editProduct } = useProducts();
+  const [product, setProduct] = useState({
+    title: "",
+    price: 0,
+    image: "",
+    category: "",
+    description: "",
+  });
+
+  const nav = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    getOneProduct(id!);
+  }, []);
+
+  useEffect(() => {
+    oneProduct && setProduct(oneProduct);
+  }, [oneProduct]);
+
+  const handleChange = (e: any) => {
+    setProduct({
+      ...product,
+      [e.target.name]: e.target.value,
+    });
+    console.log(product);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    editProduct(id!, product);
+    nav("/shop");
+  };
+
+  return (
+    <div>
+      <h2>Edit form</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={product.title}
+          placeholder="Title"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={product.category}
+          placeholder="Category"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={product.description}
+          placeholder="Description"
+          onChange={handleChange}
+        />
+        <input
+          type="url"
+          value={product.image}
+          placeholder="Image"
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          value={product.price}
+          placeholder="Price"
+          onChange={handleChange}
+        />
+      </form>
+      <button type="submit">Save</button>
+    </div>
+  );
 };
 
 export default EditShopPage;
--- src/router/MainRoutes.tsx
+++ src/router/MainRoutes.tsx
@@ -3,9 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import ShopPage from "../pages/shop/ShopPage";
 import RegisterPage from "../pages/auth/RegisterPage";
 import LoginPage from "../pages/auth/LoginPage";
-import EditShopPage from "../pages/shop/EditShopPage";
 import AddShopPage from "../pages/shop/AddShopPage";
 import HomePage from "../pages/HomePage";
+import EditShopPage from "../pages/shop/EditShopPage";
 
 const MainRoutes = () => {
   return (
@@ -13,6 +13,7 @@ const MainRoutes = () => {
       <Routes>
         <Route path="/shop" element={<ShopPage />} />
         <Route path="/shop/add" element={<AddShopPage />} />
+        <Route path="/shop/edit/:id" element={<EditShopPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<HomePage />}/>
