939b3dd5abb12e7887c1d447ab3c1b81d2b1bede
Анна Рачкова <annarachkova@MacBook-Air-Anna.local>
2023-12-04 18:52:36 +0600

    pull

--- src/router/MainRoutes.tsx
+++ src/router/MainRoutes.tsx
@@ -3,26 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import ShopPage from "../pages/shop/ShopPage";
 import RegisterPage from "../pages/auth/RegisterPage";
 import LoginPage from "../pages/auth/LoginPage";
-import EditShopPage from "../pages/shop/EditShopPage";
 import AddShopPage from "../pages/shop/AddShopPage";
 import HomePage from "../pages/HomePage";
+import { checkUserLogin } from "../helpers/functions";
+import EditShopPage from "../pages/shop/EditShopPage";
 
 const MainRoutes = () => {
   return (
     <div>
       <Routes>
         <Route path="/shop" element={<ShopPage />} />
-        <Route path="/shop/add" element={<AddShopPage />} />
+        <Route path="/shop/addshop" element={checkUserLogin() ? <AddShopPage /> : null}  />
+        <Route path="/shop/editshop" element={checkUserLogin() ? <EditShopPage /> : null}  />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<HomePage />}/>
-        {/* <Route
-          path="/add"
-          element={
-            isAdmin() ? <AddShopPage /> : null
-          }
-        ></Route>
-        <Route path="/edit/:id" element={isAdmin() ? <EditShopPage /> : null} /> */}
       </Routes>
     </div>
   );
